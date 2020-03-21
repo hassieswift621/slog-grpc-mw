7a5c5e3061c8a6e34a98afa665379383bfa4a388
hassieswift621 <hey@hassie.ninja>
2020-03-21 12:27:18 +0000

    Add options

---
+++ options.go
@@ -0,0 +1,125 @@
+package grpc_slog
+
+import (
+	"cdr.dev/slog"
+	grpc_logging "github.com/grpc-ecosystem/go-grpc-middleware/logging"
+	"google.golang.org/grpc/codes"
+	"time"
+)
+
+var (
+	defaultOptions = &options{
+		shouldLog:    grpc_logging.DefaultDeciderMethod,
+		codeFunc:     grpc_logging.DefaultErrorToCode,
+		durationFunc: DefaultDurationToField,
+	}
+)
+
+type options struct {
+	levelFunc    CodeToLevel
+	shouldLog    grpc_logging.Decider
+	codeFunc     grpc_logging.ErrorToCode
+	durationFunc DurationToField
+}
+
+type Option func(*options)
+
+// CodeToLevel function defines the mapping between gRPC return codes and interceptor log level.
+type CodeToLevel func(code codes.Code, logger slog.Logger) slog.Logger
+
+// DurationToField function defines how to produce duration fields for logging
+type DurationToField func(duration time.Duration) slog.Field
+
+func evaluateServerOpt(opts []Option) *options {
+	optCopy := &options{}
+	*optCopy = *defaultOptions
+	optCopy.levelFunc = DefaultCodeToLevel
+	for _, o := range opts {
+		o(optCopy)
+	}
+	return optCopy
+}
+
+func evaluateClientOpt(opts []Option) *options {
+	optCopy := &options{}
+	*optCopy = *defaultOptions
+	optCopy.levelFunc = DefaultClientCodeToLevel
+	for _, o := range opts {
+		o(optCopy)
+	}
+	return optCopy
+}
+
+// WithDecider customizes the function for deciding if the gRPC interceptor logs should log.
+func WithDecider(f grpc_logging.Decider) Option {
+	return func(o *options) {
+		o.shouldLog = f
+	}
+}
+
+// WithLevels customizes the function for mapping gRPC return codes and interceptor log level statements.
+func WithLevels(f CodeToLevel) Option {
+	return func(o *options) {
+		o.levelFunc = f
+	}
+}
+
+// WithCodes customizes the function for mapping errors to error codes.
+func WithCodes(f grpc_logging.ErrorToCode) Option {
+	return func(o *options) {
+		o.codeFunc = f
+	}
+}
+
+// WithDurationField customizes the function for mapping request durations to log fields.
+func WithDurationField(f DurationToField) Option {
+	return func(o *options) {
+		o.durationFunc = f
+	}
+}
+
+// DefaultCodeToLevel is the default implementation of gRPC return codes and interceptor log level for server side.
+func DefaultCodeToLevel(code codes.Code, logger slog.Logger) slog.Logger {
+	switch code {
+	case codes.OK, codes.Canceled, codes.InvalidArgument, codes.NotFound, codes.AlreadyExists, codes.Unauthenticated:
+		return logger.Leveled(slog.LevelInfo)
+	case codes.DeadlineExceeded, codes.PermissionDenied, codes.ResourceExhausted, codes.FailedPrecondition, codes.Aborted, codes.OutOfRange, codes.Unavailable:
+		return logger.Leveled(slog.LevelWarn)
+	case codes.Unknown, codes.Unimplemented, codes.Internal, codes.DataLoss:
+		return logger.Leveled(slog.LevelError)
+	default:
+		return logger.Leveled(slog.LevelError)
+	}
+}
+
+// DefaultClientCodeToLevel is the default implementation of gRPC return codes to log levels for client side.
+func DefaultClientCodeToLevel(code codes.Code, logger slog.Logger) slog.Logger {
+	switch code {
+	case codes.OK, codes.Canceled, codes.InvalidArgument, codes.NotFound, codes.AlreadyExists, codes.ResourceExhausted, codes.FailedPrecondition, codes.Aborted, codes.OutOfRange:
+		return logger.Leveled(slog.LevelDebug)
+	case codes.Unknown, codes.DeadlineExceeded, codes.PermissionDenied, codes.Unauthenticated:
+		return logger.Leveled(slog.LevelInfo)
+	case codes.Unimplemented, codes.Internal, codes.Unavailable, codes.DataLoss:
+		return logger.Leveled(slog.LevelWarn)
+	default:
+		return logger.Leveled(slog.LevelInfo)
+	}
+}
+
+// DefaultDurationToField is the default implementation of converting request duration to a slog field.
+var DefaultDurationToField = DurationToTimeMillisField
+
+// DurationToTimeMillisField converts the duration to milliseconds and uses the key `grpc.time_ms`.
+func DurationToTimeMillisField(duration time.Duration) slog.Field {
+	return slog.Field{Name: "grpc.time_ms", Value: durationToMilliseconds(duration)}
+}
+
+// DurationToDurationField uses a Duration field to log the request duration
+// and leaves it up to Log's encoder settings to determine how that is output.
+func DurationToDurationField(duration time.Duration) slog.Field {
+	return slog.Field{Name: "grpc.duration", Value: duration}
+}
+
+func durationToMilliseconds(duration time.Duration) float32 {
+	return float32(duration.Nanoseconds()/1000) / 1000
+}
